691971e1833b916646dde91281a2190d823aab1f
scherzer <scherzer@hs-weingarten.de>
2018-04-28 09:00:35 +0200

    quick commit

--- WpfOpenGlControl/WpfOpenGLControl.xaml.cs
+++ WpfOpenGlControl/WpfOpenGLControl.xaml.cs
@@ -3,29 +3,18 @@ using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 using System;
 using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Forms.Integration;
+using System.Windows.Media;
 
 namespace WpfOpenGlControl
 {
 	/// <summary>
-	/// A WPF gl control. The render context is shared between the instances. It uses a OpenTk.GLControl internally
+	/// A WPF GL control. The render context is shared between the instances. It uses a OpenTk.GLControl internally
 	/// </summary>
 	public partial class WpfOpenGLControl : UserControl
 	{
-		/// <summary>
-		/// Event is called each time the control has to be redrawn.
-		/// </summary>
-		public event EventHandler GlRender;
-
-		/// <summary>
-		/// Gets a value indicating whether the OpenGL context is valid.
-		/// </summary>
-		/// <value>
-		///   <c>true</c> if the OpenGL context is valid; otherwise, <c>false</c>.
-		/// </value>
-		public bool ValidOpenGLContext => !(s_context is null);
-
 		/// <summary>
 		/// Creates the internal glControl and attaches it to a WindowsFormsHost
 		/// </summary>
@@ -37,11 +26,44 @@ namespace WpfOpenGlControl
 			{
 				glControl = new GLControl();
 				glControl.HandleCreated += GlControl_HandleCreated;
-				windowsFormsHost1.Child = glControl;
+				host.Child = glControl;
 				WindowsFormsHost.EnableWindowsFormsInterop();
 				glControl.Paint += GlControl_Paint;
+				glControl.Resize += GlControl_Invalidate;
 			}
 		}
+
+		/// <summary>
+		/// Event is called each time the control has to be redrawn.
+		/// </summary>
+		public event EventHandler GlRender;
+
+		/// <summary>
+		/// Gets or sets a value indicating whether the render loop is activated.
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if the render loop is activated; otherwise, <c>false</c>.
+		/// </value>
+		public bool IsRenderLoopActivated
+		{
+			get { return (bool)GetValue(IsRenderLoopActivatedProperty); }
+			set { SetValue(IsRenderLoopActivatedProperty, value); }
+		}
+
+		/// <summary>
+		/// The render loop activated property
+		/// </summary>
+		public static readonly DependencyProperty IsRenderLoopActivatedProperty =
+			DependencyProperty.Register(nameof(IsRenderLoopActivated), typeof(bool), typeof(WpfOpenGLControl)
+			, new PropertyMetadata(false, OnRenderLoopActivatedPropertyChanged));
+
+		/// <summary>
+		/// Gets a value indicating whether the OpenGL context is valid.
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if the OpenGL context is valid; otherwise, <c>false</c>.
+		/// </value>
+		public bool ValidOpenGLContext => !(s_context is null);
 
 		/// <summary>
 		/// Forces a redraw of this instance.
@@ -63,12 +85,6 @@ namespace WpfOpenGlControl
 			}
 		}
 
-		private void GlControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
-		{
-			if (glControl is null) return;
-			glControl.Invalidate();
-		}
-
 		private void GlControl_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
 		{
 			if (glControl is null) return;
@@ -76,6 +92,26 @@ namespace WpfOpenGlControl
 			GL.Viewport(0, 0, glControl.Width, glControl.Height);
 			GlRender?.Invoke(this, e);
 			glControl.SwapBuffers();
+		}
+
+		private void GlControl_Invalidate(object sender, EventArgs e)
+		{
+			glControl.Invalidate();
+		}
+
+		private static void OnRenderLoopActivatedPropertyChanged(DependencyObject source, DependencyPropertyChangedEventArgs e)
+		{
+			var control = source as WpfOpenGLControl;
+			if (control is null) return;
+			if (!control.ValidOpenGLContext) return;
+			if ((bool)e.NewValue)
+			{
+				CompositionTarget.Rendering += control.GlControl_Invalidate; // render every frame
+			}
+			else
+			{
+				CompositionTarget.Rendering -= control.GlControl_Invalidate;
+			}
 		}
 	}
 }
--- examples/WpfOpenGlControl/MainWindow.xaml.cs
+++ examples/WpfOpenGlControl/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using OpenTK.Graphics.OpenGL;
-using System;
+using System.Diagnostics;
 using System.Windows;
-using System.Windows.Threading;
 
 namespace Example
 {
@@ -15,21 +14,14 @@ namespace Example
 			InitializeComponent();
 			glControl.GlRender += (s, e) => Draw(1f, 0f, 0f);
 			glControl1.GlRender += (s, e) => Draw(0f, 1f, 0f);
-			glControl2.GlRender += (s, e) => Draw(0f, 0f, 1f);
-			glControl3.GlRender += (s, e) => Draw(1f, 0f, 1f);
-			timer = new DispatcherTimer
-			{
-				Interval = TimeSpan.FromSeconds(1.0 / 60.0)
-			};
-			timer.Tick += (s, e) => Redraw();
-			timer.Start();
+			time.Start();
 		}
 
-		private float angle = 0f;
-		private readonly DispatcherTimer timer;
+		private Stopwatch time = new Stopwatch();
 
 		private void Draw(float r, float g, float b)
 		{
+			var angle = (float)time.Elapsed.TotalSeconds * 20f;
 			GL.ClearColor(r, g, b, 1.0f);
 			GL.Clear(ClearBufferMask.ColorBufferBit);
 			GL.Color3(1f, 1f, 1f);
@@ -40,14 +32,6 @@ namespace Example
 			GL.Vertex2(.9, -.9);
 			GL.Vertex2(.9, .9);
 			GL.End();
-		}
-
-		private void Redraw()
-		{
-			angle += (float)timer.Interval.TotalSeconds * 100;
-			glControl.Invalidate();
-			glControl2.Invalidate();
-			glControl3.Invalidate();
 		}
 	}
 }
